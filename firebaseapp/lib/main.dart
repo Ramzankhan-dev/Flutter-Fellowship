a4ef7b5f5890ee9c5846a5a5a78cc9902632755f
MuhammadRamzan <sp23-bse-027@cuiatk.edu.pk>
2026-03-04 14:39:14 +0500

    add queue for msg when no internet

--- firebaseapp/lib/main.dart
+++ firebaseapp/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'login_screen.dart';
@@ -5,8 +6,9 @@ import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  FirebaseFirestore.instance.settings = const Settings(
+    persistenceEnabled: true, // ← Ye add karo
   );
   runApp(const MyApp());
 }
